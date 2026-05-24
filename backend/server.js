5af8028968896f69a3a849507ee174abf90c499d
fionnaflrntia <fionnaflorentia@gmail.com>
2026-05-24 22:35:39 +0700

    update

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,6 @@ connectDB()
 app.use(cors({
   origin: [
     'http://localhost:5173',
-    'https://dapur-mama-web.vercel.app',
     'https://dapur-mama-web-amber.vercel.app/'
   ],
   credentials: true,
