8132616ee0e9b676ca3f68971b0be48006fa9df2
fionnaflrntia <fionnaflorentia@gmail.com>
2026-05-24 22:55:24 +0700

    fix

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,33 +1,12 @@
 const mongoose = require('mongoose')
 
-let cached = global.mongoose
-
-if (!cached) {
-  cached = global.mongoose = {
-    conn: null,
-    promise: null,
-  }
-}
-
 const connectDB = async () => {
-  if (cached.conn) {
-    return cached.conn
-  }
-
-  if (!cached.promise) {
-    cached.promise = mongoose.connect(process.env.MONGO_URI, {
-      bufferCommands: false,
-    })
-  }
-
   try {
-    cached.conn = await cached.promise
-    console.log(`MongoDB Connected: ${cached.conn.connection.host}`)
-    return cached.conn
+    const conn = await mongoose.connect(process.env.MONGO_URI)
+    console.log(`MongoDB Connected: ${conn.connection.host}`)
   } catch (error) {
-    cached.promise = null
     console.error(`MongoDB Error: ${error.message}`)
-    throw error
+    process.exit(1)
   }
 }
 
